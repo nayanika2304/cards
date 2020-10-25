e0714677a035bc41e974e46c639e6442866354ef
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-10-25 14:59:30 -0400

    finished the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content : 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content : 'Here is the picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content : 'I did some biking today'
+    }
+  ];
 }
